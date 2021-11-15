4f9321ab8de3578644cf155678be9e4b96e744c6
facumartinezz <fmartinezz@udesa.edu.ar>
2021-11-15 19:13:06 -0300

    linkeados

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let url = 'https://api.themoviedb.org/3/movie/76341?api_key=39567120a5a71b9d0780859871f73c82'
+fetch(url)
+.then()
+.then()
+.catch()
+
+
+let buscador = document.querySelector(".buscador-home");
+buscador.addEventListener('submit', function(){
+    if (buscador == null){
+
+    }
+
+})
