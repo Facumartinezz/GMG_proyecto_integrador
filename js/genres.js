1a085b51c8badfcf87382b4adc3c47b21f15f315
Joaquin-Guerrico <jguerrico@udesa.edu.ar>
2021-11-22 04:16:22 -0300

    generos

--- js/genres.js
+++ js/genres.js
@@ -1,7 +1,7 @@
 /* Generos de las Películas */
-let url = ("https://api.themoviedb.org/3/genre/movie/list?api_key=39567120a5a71b9d0780859871f73c82");
+let url1 = 'https://api.themoviedb.org/3/genre/movie/list?api_key=39567120a5a71b9d0780859871f73c82'
 
-fetch(url)
+fetch(url1)
     
     .then(function(response){
         return response.json();
@@ -12,17 +12,44 @@ fetch(url)
         console.log(data);
         
         for(let i = 0; i < data.genres.length; i++){
-            document.querySelector("#section-generos-peliculas").innerHTML +=
+            document.querySelector('#section-generos-peliculas').innerHTML +=
             `
-                <section id="section-generos-peliculas">
+                <article class="generopeliculas">
             
-                    <h2>géneros de películas</h2>
+                    <ul class="generos">
+                        <li><a href="comedia.html?id=${data.genres[i].id}">${data.genres[i].name}</a></li>
+                    </ul>
+                </article>
+            `
+        }
+    })
+    
+    .catch(function(error){
+        console.log('El error fue:' + error);
+    })
+
+/* Generos de las Series */
+let url2 = 'https://api.themoviedb.org/3/genre/tv/list?api_key=39567120a5a71b9d0780859871f73c82'
+
+fetch(url2)
+    
+    .then(function(response){
+        return response.json();
+    })
+    
+    .then(function(data){
+        
+        console.log(data);
+        
+        for(let i = 0; i < data.genres.length; i++){
+            document.querySelector('#section-generos-series').innerHTML +=
+            `
+                <article class="generoseries">
             
-                    <li>
-                        <a href="genres.html?id= ${data.genres[i].id}">${data.genres[i].name}</a>
-                    </li>
-                    
-                </section>
+                    <ul class="generos">
+                        <li><a href="comedia.html?id=${data.genres[i].id}">${data.genres[i].name}</a></li>
+                    </ul>
+                </article>
             `
         }
     })
